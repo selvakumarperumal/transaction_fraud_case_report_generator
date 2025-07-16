75a88eddf70a210dbfcdb5b8a9fca5cfe33b17fb
Selvakumar Perumal <selvakumarp@proton.me>
2025-07-16 13:50:32 +0530

    Module Issue Fixed

--- app/services/langchain_service.py
+++ app/services/langchain_service.py
@@ -1,10 +1,10 @@
 from langchain_huggingface.chat_models import ChatHuggingFace
 from langchain_huggingface import HuggingFaceEndpoint
-from langchain.schema.messages import HumanMessage, SystemMessage, AIMessage
-from dotenv import load_dotenv
-import os
+from langchain_core.messages import HumanMessage
+from app.core.config import Settings
 
-hf_token = os.getenv("HF_TOKEN")
+settings = Settings()
+hf_token = settings.HUGGINGFACE_API_TOKEN
 
 # Create base LLM first
 llm = HuggingFaceEndpoint(
@@ -22,6 +22,6 @@ messages = [
     HumanMessage(content="What is the capital of France?, and why?"),
 ]
 
-response = chat_model.invoke(input=messages)
+response = chat_model.invoke(messages)
 
-print(response)
+print(response.content)
